797c6872d85a54664569f939e8d15accc2cc1f63
aelassas <akram.elassas@gmail.com>
2025-07-07 20:11:41 +0100

    feat: add vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
-export default {
+import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+
+export default defineConfig({
   base: '/',
-}
+  plugins: [
+    createHtmlPlugin({
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+  ],
+})
