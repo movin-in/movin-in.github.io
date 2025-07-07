4ab560fbb953af343dbe8c04429bc6207ec95bf0
aelassas <akram.elassas@gmail.com>
2025-07-07 17:04:49 +0100

    refactor: github star button

--- src/js/main.js
+++ src/js/main.js
@@ -5,21 +5,17 @@ import {
   initLanguageMenu,
   initThemeToggle,
   updateDownloadLink,
-  loadGitHubButtonsScript,
-  initGitHubButton,
   updateFooterYear
 } from './ui.js'
 
 window.addEventListener('DOMContentLoaded', async () => {
   try {
-    await loadGitHubButtonsScript()
     await loadInitialLanguage()
     initHamburgerMenu()
     initHeaderScroll()
     initLanguageMenu(setLang)
     initThemeToggle()
     await updateDownloadLink()
-    initGitHubButton()
     updateFooterYear()
   } catch (err) {
     console.error('Error during app initialization:', err)
